0a320943ab026397c3b6097699525ee31014a58e
Diego <fdiegofreire@gmail.com>
2024-09-01 11:11:03 -0300

    ajuste erro do form post duplicado e finalização

--- matricula/views.py
+++ matricula/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .form import *
 # Create your views here.
 
 def index(request):
     alunos=Aluno.objects.all()
-    if request.method=='POST':
+    if request.method =='POST':
         form=AlunoForm(request.POST)
         if form.is_valid():
             form.save()
-            form=AlunoForm()
+            return redirect('index') ##redirecionando para a página novamente para o caso de o navegador enviar o request.POST repetidamente(erro que estava acontecendo)
     else:
         form=AlunoForm()
 
